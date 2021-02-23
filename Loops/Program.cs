3a83e57655c470d8bce44c9702ebed25abb0afa1
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-23 15:49:25 -0800

    added while loops'

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -17,21 +17,40 @@ namespace Loops
             }
 
             //Do While Loops
-            int hello = 0;
-            int lengthText = 0;
-            do
+            //int hello = 0;
+            //int lengthText = 0;
+            //do
+            //{
+            //    Console.WriteLine("Please enter the name of your friend: ");
+            //    string friend = Console.ReadLine();
+            //    int currentLength = friend.Length;
+            //    lengthText += currentLength;
+
+            //    Console.WriteLine(hello + "hello");
+            //    hello++;
+            //} while (hello < 20);
+            //Console.WriteLine("Thanks, that's engouh!");
+
+            //while loop
+
+            int counter = 0;
+            while(counter < 10)
             {
-                Console.WriteLine("Please enter the name of your friend: ");
-                string friend = Console.ReadLine();
-                int currentLength = friend.Length;
-                lengthText += currentLength;
+                Console.WriteLine(counter);
+                counter++;
+            }
 
-                Console.WriteLine(hello + "hello");
-                hello++;
-            } while (hello < 20);
-            Console.WriteLine("Thanks, that's engouh!");
+            int people = 0;
+            string enteredText = "";
+            while (enteredText.Equals(""))
+            {
+                Console.WriteLine("Please press enter to increse amount by one and anything else + enter if you want to finish counting.");
+                enteredText = Console.ReadLine();
+
+                people++;
+                Console.WriteLine("Current people count is {0}", people);
+            }
             Console.Read();
-
 
         }
     }
