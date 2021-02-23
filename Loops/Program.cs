a438eebbc6f9a0e73da20e9f96b4802c20e061f5
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-23 12:57:35 -0800

    added a few for loops

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -6,7 +6,16 @@ namespace Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //For loops
+            for (int i = 0; i <= 10; i++){
+                Console.WriteLine(i + "i");
+            }
+
+            for (int count = 0; count < 20; count += 2)
+            {
+                Console.WriteLine(count + "count");
+            }
+            
         }
     }
 }
